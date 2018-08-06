1c5e0a38c2ecd53710811715bf52efb2ec28e0b5
DeepDagon <31501846+DeepDagon@users.noreply.github.com>
2018-08-07 00:07:43 +0500

    Add files via upload

---
+++ Constant.py
@@ -0,0 +1,16 @@
+#Window
+display_width = 1024 
+display_height = 720
+display_title = "ЭкстраАлхимия"
+
+#Player
+animCount = 0 #Для анимации
+x = 50 
+y = 425
+width = 60
+height = 71
+speed = 5
+
+#Move
+left = False
+right = False
