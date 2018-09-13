cb800c306575ed6ea468350478941811b8bc60d9
Unknown <vladdd183@gmail.com>
2018-09-13 16:53:43 +0300

    Refractoring

--- gadgets.py
+++ gadgets.py
@@ -6,7 +6,7 @@ from pygame.image import load
 class lamp(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = load('images/lamp/lampOn.png')
+        self.image = load('images/lamp/LampOn.png')
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -15,7 +15,7 @@ class lamp(pygame.sprite.Sprite):
 class bucket(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = load('images/bucket/bucketFull.png')
+        self.image = load('images/bucket/BucketFull.png')
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
+
 from threading import Thread
 import time
 import random
 import pygame
 from pygame.locals import *
-from player import *
+from Player import *
 from plants import *
 from gadgets import *
 from Buttons import *
@@ -14,9 +16,13 @@ pygame.mixer.pre_init(44100, -16, 2, 1024)
 pygame.mixer.init()
 
 # Настройка игрового окна
-SIZE = (1366, 768)  # Группируем ширину и высоту в одну переменную
+SIZE = (1280, 1024)  # Группируем ширину и высоту в одну переменную (можно указать любые)
+
+x = SIZE[0]/100
+y = SIZE[1]/100
+
 background_one = 'images/backgrounds/background_one.png'  # Фон первого уровня
-background_night_path = 'images/backgrounds/night.png'  # Путь до ночной маски
+background_night_path = 'images/backgrounds/Night.png'  # Путь до ночной маски
 win_title = 'ЭкстраАлхимия'
 screen = pygame.display.set_mode((SIZE))  # Размеры окна
 pygame.display.set_caption(win_title)  # Надпись вверху окна
@@ -36,22 +42,25 @@ soundtrack_night = pygame.mixer.Sound('sound/Soundtrack/night.ogg')
 soundtrack_cook = pygame.mixer.Sound('sound/Soundtrack/cook.ogg')
 
 # звуки
-night_start = pygame.mixer.Sound('sound/night start/1.ogg')
-night_end = pygame.mixer.Sound('sound/night end/1.ogg')
-sound_end = pygame.mixer.Sound('sound/start round/1.ogg')
-sound_taking = pygame.mixer.Sound('sound/green/1.ogg')
-sound_lamp = pygame.mixer.Sound('sound/lights/1.ogg')
-sound_bucket = pygame.mixer.Sound('sound/water/1.ogg')
-sound_umbrella = pygame.mixer.Sound('sound/umbrella/1.ogg')
-sound_click = pygame.mixer.Sound('sound/click/1.ogg')
+night_start = pygame.mixer.Sound('sound/Night_start/1.ogg')
+night_end = pygame.mixer.Sound('sound/Night_end/1.ogg')
+sound_end = pygame.mixer.Sound('sound/Start_round/1.ogg')
+sound_taking = pygame.mixer.Sound('sound/Green/1.ogg')
+sound_lamp = pygame.mixer.Sound('sound/Lights/1.ogg')
+sound_bucket = pygame.mixer.Sound('sound/Water/1.ogg')
+sound_umbrella = pygame.mixer.Sound('sound/Umbrella/1.ogg')
+sound_click = pygame.mixer.Sound('sound/Click/1.ogg')
 
 # Настройка шрифтов
-my_font = pygame.font.Font(None, 72)
+my_font = pygame.font.Font(None, 70)
 irina_font = pygame.font.SysFont('IrinaCCT', 64)
 gadget_font = pygame.font.SysFont('IrinaCCT', 52)
+small_font = pygame.font.SysFont('IrinaCCT', 40)
 
 # Создание героя
-hero = Player(550, 550)
+hero_x = SIZE[0]/3
+hero_y = SIZE[1]/3
+hero = Player(hero_x, hero_y)
 left = right = up = down = False
 
 # Группируем спрайты
@@ -60,41 +69,20 @@ plantslist = []  # Список со всеми сгенерированными
 
 
 def genworld():
-    i = 0
-    XPos = 200
-    YPos = 140
+    XPos = SIZE[0]/6
+    YPos = SIZE[1]/6
 
-    while i < 5:
-        PlantsRender = sunPlants(XPos, YPos)
-        sprite_group.add(PlantsRender)
-        plantslist.append(PlantsRender)
+    PlantsRender = []
 
-        YPos += 110
-        i += 1
+    for i in range(1,6):
+        PlantsRender.append(sunPlants(XPos, YPos*i))
+        PlantsRender.append(shadowPlants(XPos*3, YPos*i))
+        PlantsRender.append(waterPlants(XPos*5, YPos*i))
 
-    i = 0
-    XPos = 650
-    YPos = 140
+    for plant in PlantsRender:
+        sprite_group.add(plant)
+        plantslist.append(plant)
 
-    while i < 5:
-        PlantsRender = shadowPlants(XPos, YPos)
-        sprite_group.add(PlantsRender)
-        plantslist.append(PlantsRender)
-
-        YPos += 110
-        i += 1
-
-    i = 0
-    XPos = 1100
-    YPos = 140
-
-    while i < 5:
-        PlantsRender = waterPlants(XPos, YPos)
-        sprite_group.add(PlantsRender)
-        plantslist.append(PlantsRender)
-
-        YPos += 110
-        i += 1
 
 
 genworld()
@@ -107,16 +95,18 @@ bucketNumber = 5
 
 #Рендер всего
 def baserender():
+    color = (253, 234, 168)
     # Установка фонового изображения (поле)
     screen.blit(background_image, (0, 0))
-    screen.blit(my_font.render(time_info, 1, (253, 234, 168)), (400, 20))
+    screen.blit(my_font.render(time_info, 10, color), (x*30, y*2))
 
-    screen.blit(gadget_font.render(umbrellaInfo, 1, (253, 234, 168)), (10, 10))   
-    screen.blit(gadget_font.render(lampInfo, 1, (253, 234, 168)), (10, 50))   
-    screen.blit(gadget_font.render(bucketInfo, 1, (253, 234, 168)), (10, 90)) 
+    screen.blit(gadget_font.render(umbrellaInfo, 1, color), (x, y))
+    screen.blit(gadget_font.render(lampInfo, 1, color), (x, y*5))
+    screen.blit(gadget_font.render(bucketInfo, 1, color), (x, y*10))
 
     hero.update(left, right, up, down, plantslist)
     sprite_group.draw(screen)
+
 
 
 seconds = -1
@@ -134,7 +124,6 @@ NumberWaterPlants = 0
 
 def seconds_counter():
     global seconds, sunTime, shadowTime, waterTime
-
     while True:
         seconds += 1
         sunTime += 1
@@ -186,14 +175,14 @@ while isRunning:
     sunNumberInfo = u'Время истекло, вы собрали ' + str(NumberSunPlants) + ' солнечных растений'
     shadowNumberInfo = str(NumberShadowPlants) + u' сумеречных растений'
     waterNumberInfo = str(NumberWaterPlants) + u' водных растений'
-    
+
     #Информация о количестве гаджетов
     umbrellaInfo = u'Зонтиков: ' + str(umbrellaNumber)
-    lampInfo = u'Ламп: ' + str(lampNumber) 
+    lampInfo = u'Ламп: ' + str(lampNumber)
     bucketInfo = u'Вёдер: ' + str(bucketNumber)
 
     for event in pygame.event.get():
-        if event.type == pygame.locals.QUIT or (event.type == pygame.locals.KEYDOWN and (event.key == pygame.locals.K_ESCAPE)):
+        if (event.type == pygame.locals.QUIT) or ((event.type == pygame.locals.KEYDOWN) and (event.key == pygame.locals.K_ESCAPE)):
             pygame.quit()
             exit()
 
@@ -217,21 +206,21 @@ while isRunning:
             if event.key == pygame.K_DOWN:
                 down = False
 
-            if event.key == pygame.K_l and lampNumber > 0 and seconds in range (100, 201):
+            if (event.key == pygame.K_l) and (lampNumber > 0) and (seconds in range(100, 201)):
                 PlantsRender = lamp(playerPositionX, playerPositionY)
                 sprite_group.add(PlantsRender)
                 sound_lamp.play()
                 lampNumber -= 1
                 sunTime +=5
 
-            if event.key == pygame.K_b and bucketNumber > 0:
+            if (event.key == pygame.K_b) and (bucketNumber > 0):
                 PlantsRender = bucket(playerPositionX, playerPositionY)
                 sprite_group.add(PlantsRender)
                 sound_bucket.play()
                 bucketNumber -= 1
                 waterTime += 5
 
-            if event.key == pygame.K_u and umbrellaNumber > 0 and seconds in range(0, 101):
+            if (event.key == pygame.K_u) and ((umbrellaNumber > 0) and (seconds in range(0, 101))):
                 PlantsRender = umbrella(playerPositionX, playerPositionY)
                 sprite_group.add(PlantsRender)
                 sound_umbrella.play()
@@ -239,7 +228,7 @@ while isRunning:
                 shadowTime +=5
 
             if event.key == pygame.K_e:
-                if waterTime in range (80, 101):
+                if waterTime in range(80, 101):
                     waterTime = 201
                     NumberWaterPlants += 5
                     sound_taking.play()
@@ -254,17 +243,19 @@ while isRunning:
                     NumberSunPlants += 5
                     sound_taking.play()
 
-    if seconds in range (0, 100):
+    color = (253, 234, 168)
+
+    if seconds in range(0, 100):
         baserender()
         if soundValue == 1:
             soundtrack_day.play()
             soundValue = 2
 
-        if waterTime in range (80, 100):
+        if waterTime in range(80, 100):
             screen.blit(my_font.render(
-                'Скорее собирай водные растения!', 1, (253, 234, 168)), (210, 70))
+                'Скорее собирай водные растения!', 1, color), (x*20, y*10))
 
-    elif seconds in range (100, 201):
+    elif seconds in range(100, 201):
         baserender()
         if soundValue == 2:
             night_start.play()
@@ -273,11 +264,11 @@ while isRunning:
 
         if shadowTime in range(140, 180):
             screen.blit(my_font.render(
-                'Скорее собирай сумеречные растения!', 1, (253, 234, 168)), (200, 70))
+                'Скорее собирай сумеречные растения!', 1, color), (x*20, y*10))
 
         if sunTime in range(180, 200):
             screen.blit(my_font.render(
-                'Скорее собирай солнечные растения!', 1, (253, 234, 168)), (200, 70))
+                'Скорее собирай солнечные растения!', 1, color), (x*20, y*10))
 
         screen.blit(night_mask, (0, 0)) #Ночь
 
@@ -291,30 +282,34 @@ while isRunning:
             soundValue = 0
 
         screen.blit(cook_level, (0, 0))
-        screen.blit(irina_font.render(sunNumberInfo,
-                                      1, (253, 234, 168)), (150, 30))
-        screen.blit(irina_font.render(shadowNumberInfo,
-                                      1, (253, 234, 168)), (450, 80))
-        screen.blit(irina_font.render(waterNumberInfo,
-                                      1, (253, 234, 168)), (500, 130))
+        screen.blit(small_font.render(sunNumberInfo,
+                                      1, color), (x*15, y*3))
+        screen.blit(small_font.render(shadowNumberInfo,
+                                      1, color), (x*44, y*7))
+        screen.blit(small_font.render(waterNumberInfo,
+                                      1, color), (x*48, y*12))
 
-        desertButton.create_button(screen, (107, 142, 35), 300, 200, 250, 100, 0,
-                              "Пустынное зелье", (255, 255, 255))
-        sunsetButton.create_button(screen, (107, 142, 35), 600, 200, 250, 100, 0,
-                              "Закатное зелье", (255, 255, 255))
-        fogButton.create_button(screen, (107, 142, 35), 900, 200, 250, 100, 0,
-                              "Туманное зелье", (255, 255, 255))
-        invisibleButton.create_button(screen, (107, 142, 35), 300, 350, 250, 100, 0,
-                              "Зелье невидимости", (255, 255, 255))
-        swampButton.create_button(screen, (107, 142, 35), 600, 350, 250, 100, 0,
-                              "Болотное зелье", (255, 255, 255))
-        underwaterButton.create_button(screen, (107, 142, 35), 900, 350, 250, 100, 0,
-                              "Подводное зелье", (255, 255, 255))
-        stopCookButton.create_button(screen, (107, 142, 35), 600, 500, 250, 100, 0,
-                              "Закончить варку", (255, 255, 255))
+        color = (107, 142, 35)
+        white_color = (255, 255, 255)
+        desertButton.create_button(screen, color, x*10, y*25, 250, 100, 0,
+                              "Пустынное зелье", white_color)
+        sunsetButton.create_button(screen, color, x*30, y*25, 250, 100, 0,
+                              "Закатное зелье", white_color)
+        fogButton.create_button(screen, color, x*50, y*25, 250, 100, 0,
+                              "Туманное зелье", white_color)
+        invisibleButton.create_button(screen, color, x*10, y*45, 250, 100, 0,
+                              "Зелье невидимости", white_color)
+        swampButton.create_button(screen, color, x*30, y*45, 250, 100, 0,
+                              "Болотное зелье", white_color)
+        underwaterButton.create_button(screen, color, x*50, y*45, 250, 100, 0,
+                              "Подводное зелье", white_color)
+        stopCookButton.create_button(screen, color, x*30, y*65, 250, 100, 0,
+                              "Закончить варку", white_color)
 
-        screen.blit(irina_font.render(DSFnumber, 1, (253, 234, 168)), (300, 620))   
-        screen.blit(irina_font.render(ISUnumber, 1, (253, 234, 168)), (290, 680))
+        color = (253, 234, 168)
+
+        screen.blit(small_font.render(DSFnumber, 1, color), (x*30, y*80))
+        screen.blit(small_font.render(ISUnumber, 1, color), (x*30, y*90))
 
         for event in pygame.event.get():
             if event.type == pygame.locals.QUIT or (event.type == pygame.locals.KEYDOWN
@@ -325,7 +320,7 @@ while isRunning:
             if event.type == MOUSEBUTTONDOWN:
                 if desertButton.pressed(pygame.mouse.get_pos()) and NumberSunPlants >= 2:
                     NumberSunPlants -= 2
-                    desertNumber += 1 
+                    desertNumber += 1
                     sound_click.play()
                     money += 4
 
@@ -365,38 +360,39 @@ while isRunning:
                     money += 5
 
                 if stopCookButton.pressed(pygame.mouse.get_pos()):
-                    seconds = 201 
+                    seconds = 201
                     ThirdStage = True
                     sound_end.play()
 
         if ThirdStage:
+            color = (253, 234, 168)
             screen.blit(cook_level, (0,0))
-            screen.blit(irina_font.render('Спасибо за прохождение игры!', 1, (253, 234, 168)), (350, 50))
-            screen.blit(irina_font.render('Разработчик: vk.com/val_kd', 1, (253, 234, 168)), (350, 100))
-            screen.blit(irina_font.render('Композитор: vk.com/pointerwar', 1, (253, 234, 168)), (350, 150))
-            screen.blit(irina_font.render('Графика: vk.com/20usa22', 1, (253, 234, 168)), (350, 200))
-            screen.blit(irina_font.render('Группа игры: vk.com/fastprogram', 1, (253, 234, 168)), (350, 250))
+            screen.blit(irina_font.render('Спасибо за прохождение игры!', 1, color), (x*15, y*5))
+            screen.blit(irina_font.render('Разработчик: vk.com/val_kd', 1, color), (x*15, y*10))
+            screen.blit(irina_font.render('Композитор: vk.com/pointerwar', 1, color), (x*15, y*15))
+            screen.blit(irina_font.render('Графика: vk.com/20usa22', 1, color), (x*15, y*20))
+            screen.blit(irina_font.render('Группа игры: vk.com/fastprogram', 1, color), (x*15, y*25))
 
             if seconds > 200:
                 if money == 0:
                     moneyInfo = 'Вы ничего не продали'
-                    screen.blit(irina_font.render(moneyInfo, 1, (253, 234, 168)), (450, 360))
+                    screen.blit(irina_font.render(moneyInfo, 1, color), (x*25, y*36))
 
                 elif money == 1 or money == 21:
                     moneyInfo = 'Вы получили за зелья: ' + str(money) + ' золотую монету'
-                    screen.blit(irina_font.render(moneyInfo, 1, (253, 234, 168)), (280, 360))
+                    screen.blit(irina_font.render(moneyInfo, 1, color), (x*15, y*36))
 
-                elif money in range(2, 5) or money in range(22, 25): 
+                elif money in range(2, 5) or money in range(22, 25):
                     moneyInfo = 'Вы получили за зелья: ' + str(money) + ' золотые монеты'
-                    screen.blit(irina_font.render(moneyInfo, 1, (253, 234, 168)), (280, 360))
+                    screen.blit(irina_font.render(moneyInfo, 1, color), (x*15, y*36))
 
                 elif money in range(5, 21) or money == 25:
                     moneyInfo = 'Вы получили за зелья: ' + str(money) + ' золотых монет'
-                    screen.blit(irina_font.render(moneyInfo, 1, (253, 234, 168)), (280, 360))
+                    screen.blit(irina_font.render(moneyInfo, 1, color), (x*15, y*36))
 
                 else:
                     moneyInfo = 'Что-то пошло не так :('
-                    screen.blit(irina_font.render(moneyInfo, 1, (253, 234, 168)), (450, 360))
+                    screen.blit(irina_font.render(moneyInfo, 1, color), (x*22, y*36))
 
     else:
         print("Ошибка времени")
